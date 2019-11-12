e26120160ec19845505679cac2859952081e0e38
Mark Halls <mark.halls@gmail.com>
2019-11-12 16:19:38 -0700

    Add login form

--- app/src/components/Login/index.jsx
+++ app/src/components/Login/index.jsx
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 
 import { actions } from "../../actions";
 
-const Login = () => {
+const Login = ({history}) => {
+  const [formState, setFormState] = useState({
+    username: "Lambda School",
+    password: "i<3Lambd4"
+  });
   const dispatch = useDispatch();
-  dispatch(actions.login({ username: "Lambda School", password: "i<3Lambd4" }));
-  return <div></div>;
+
+  const handleSubmit = () => {
+    dispatch(
+      actions.login({
+        username: formState.username,
+        password: formState.password
+      })
+    );
+    history.push("/friends");
+  };
+
+  const handleChange = e => {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div>
+      <input
+        type="username"
+        name="username"
+        value={formState.username}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={formState.password}
+        onChange={handleChange}
+      />
+
+      <button type="button" onClick={() => handleSubmit()}>
+        Submit
+      </button>
+    </div>
+  );
 };
 
 export default Login;
